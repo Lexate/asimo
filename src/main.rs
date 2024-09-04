2dde3e5e3d66e6cc929337265375ba81bf6e08aa
Lexate <47445605+Lexate@users.noreply.github.com>
2024-09-04 09:10:32 +0200

    Serial port test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let mut port = serialport::new("/dev/ttyUSB0", 115_200)
+        .timeout(Duration::from_millis(10))
+        .open()
+        .expect("Failed to open port");
+
+    let output: Vec<u8> = vec![2, 129, 8, 0, 0, 144, 172, 2, 0, 0, 96, 3]; // Setsound type
+
+    port.write(&output[..]).expect("Could not write");
 }
