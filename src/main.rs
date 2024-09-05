d4aa1c0225ef1ff7d806fd25c071d6fb68d00cbb
Lexate <47445605+Lexate@users.noreply.github.com>
2024-09-05 11:07:26 +0200

    Basic error handling for asimo_comms

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,14 @@ use std::time::Duration;
 //TODO: Error handling
 
 fn main() {
-    let mut serial = asimo_comms::Serial::new("/dev/ttyUSB0", 115_200, Duration::from_millis(10));
+    let mut serial = asimo_comms::Serial::new("/dev/ttyUSB0", 115_200, Duration::from_millis(10))
+        .expect("Could not open port");
 
     let output: Vec<u8> = vec![2, 129, 8, 0, 0, 144, 172, 2, 0, 0, 96, 3]; // Setsound type (0)
 
-    serial.write(&output[..]);
+    serial.write(&output[..]).expect("Could not write");
 
-    let response = serial.read();
+    let response = serial.read().expect("Could not read");
     println!("{:?}", response);
 }
 
@@ -23,30 +24,31 @@ mod asimo_comms {
     }
 
     impl Serial {
-        pub fn new(path: &str, baud_rate: u32, timeout: Duration) -> Self {
-            let port = serialport::new(path, baud_rate)
-                .timeout(timeout)
-                .open()
-                .expect("Failed to open port ");
+        pub fn new(
+            path: &str,
+            baud_rate: u32,
+            timeout: Duration,
+        ) -> Result<Self, serialport::Error> {
+            let port = serialport::new(path, baud_rate).timeout(timeout).open()?;
 
-            Self { port }
+            Ok(Self { port })
         }
 
-        pub fn write(&mut self, output: &[u8]) {
-            self.port.write(&output[..]).expect("Could not write");
+        pub fn write(&mut self, output: &[u8]) -> Result<usize, std::io::Error> {
+            self.port.write(&output[..])
         }
 
-        pub fn read(&mut self) -> Vec<u8> {
+        pub fn read(&mut self) -> Result<(usize, Vec<u8>), std::io::Error> {
             let mut serial_buf: Vec<u8> = vec![0; 256];
-            self.port
-                .read(serial_buf.as_mut_slice())
-                .expect("Found no data");
-            serial_buf
+            let bytes_written = self.port.read(serial_buf.as_mut_slice())?;
+            Ok((bytes_written, serial_buf))
         }
 
-        pub fn write_read(&mut self, output: &[u8]) -> Vec<u8> {
-            self.write(output);
-            self.read()
+        pub fn write_read(&mut self, output: &[u8]) -> Result<Vec<u8>, std::io::Error> {
+            self.write(output)?;
+            let (_, response) = self.read()?;
+
+            Ok(response)
         }
     }
 }
