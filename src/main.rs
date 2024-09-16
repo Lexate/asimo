579ef0a4867cf91a648acf94ecb34c92d7c0ec16
Lexate <47445605+Lexate@users.noreply.github.com>
2024-09-16 16:02:02 +0200

    Refactor and provide encoding function

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,16 @@
-use asimo;
+use asimo::{proto, types};
 use std::time::Duration;
 //TODO: Error handling
 
 fn main() {
-    /*
-    let mut serial = asimo::comms::Serial::new("/dev/ttyUSB0", 115_200, Duration::from_millis(10))
-        .expect("Could not open port");
-
-    let output: Vec<u8> = vec![2, 129, 8, 0, 0, 144, 172, 2, 0, 0, 96, 3]; // Setsound type (0)
-
-    serial.write(&output[..]).expect("Could not write");
-
-    let response = serial.read().expect("Could not read");
-    println!("{:?}", response);
-    */
     println!(
-        "{}",
-        asimo::proto::calc_crc(&vec![2, 129, 8, 0, 0, 144, 172, 2, 0, 0,], 1)
+        "{:x?}",
+        proto::encode(types::inParams::SoundSetSoundType(
+            types::tSoundType::SoundKeyClick
+        ))
+    );
+    println!(
+        "{:x?}",
+        proto::encode(types::inParams::SystemSettingsSetHeadlightEnabled(1))
     );
 }
