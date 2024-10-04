f4d1e40a3f19e1b7c476f61ccec59c1c97b73437
Lexate <47445605+Lexate@users.noreply.github.com>
2024-10-04 20:30:03 +0200

    Add serial read

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@ mod gen_types;
 pub use crate::gen_types::types;
 
 pub mod comms {
-    use std::time::Duration;
-
     use serialport::SerialPort;
+    use std::{thread::sleep, time::Duration};
 
     pub struct Serial {
         port: Box<dyn SerialPort>,
@@ -21,21 +20,26 @@ pub mod comms {
             Ok(Self { port })
         }
 
-        pub fn write(&mut self, output: &[u8]) -> Result<usize, std::io::Error> {
-            self.port.write(&output[..])
+        fn read_size(&mut self, lenth: usize) -> Result<Vec<u8>, std::io::Error> {
+            let mut buf = vec![0u8; lenth];
+            match self.port.read_exact(&mut buf) {
+                Ok(_) => return Ok(buf),
+                //Err(e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+                Err(e) => return Err(e),
+            }
         }
 
-        pub fn read(&mut self) -> Result<(usize, Vec<u8>), std::io::Error> {
-            let mut serial_buf: Vec<u8> = vec![0; 256];
-            let bytes_written = self.port.read(serial_buf.as_mut_slice())?;
-            Ok((bytes_written, serial_buf))
-        }
+        pub fn send_message(&mut self, message: Vec<u8>) -> Result<Vec<u8>, std::io::Error> {
+            self.port.write(&message[..])?;
 
-        pub fn write_read(&mut self, output: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-            self.write(output)?;
-            let (_, response) = self.read()?;
-
-            Ok(response)
+            let mut buf = vec![0u8; 256];
+            loop {
+                match self.port.read(buf.as_mut_slice()) {
+                    Ok(t) => return Ok(buf[..t].to_vec()),
+                    Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+                    Err(e) => eprintln!("{:?}", e),
+                }
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,62 @@
-use asimo::{proto, types};
-use std::time::Duration;
+use asimo::{comms, proto, types};
+use std::{thread::sleep, time::Duration};
 //TODO: Error handling
 
+fn motor(left: f32, right: f32) -> Vec<u8> {
+    let left = f32::clamp(left, -1.0, 1.0);
+    let right = f32::clamp(right, -1.0, 1.0);
+
+    proto::encode(types::inParams::HardwareControlWheelMotorsPower(
+        (left * 100.0) as i16,
+        (right * 100.0) as i16,
+    ))
+    .0
+}
+
+fn arcade_drive(rot: f32, fwd: f32, square: bool) -> (f32, f32) {
+    let (rot, fwd) = if square {
+        (rot.signum() * rot * rot, fwd.signum() * fwd * fwd)
+    } else {
+        (rot, fwd)
+    };
+
+    let max = f32::max(rot.abs(), fwd.abs());
+    let (tot, diff) = (fwd + rot, fwd - rot);
+
+    if fwd >= 0.0 {
+        if rot >= 0.0 {
+            return (max, diff);
+        } else {
+            return (tot, max);
+        }
+    } else {
+        if rot >= 0.0 {
+            return (tot, -max);
+        } else {
+            return (-max, diff);
+        }
+    }
+}
+
 fn main() {
-    println!(
-        "{:x?}",
-        proto::encode(types::inParams::SoundSetSoundType(
-            types::tSoundType::SoundKeyClick
-        ))
-    );
-    println!(
-        "{:x?}",
-        proto::encode(types::inParams::SystemSettingsSetHeadlightEnabled(1))
-    );
+    let mut serial = comms::Serial::new("/dev/ttyUSB0", 115200, Duration::from_millis(1))
+        .expect("Could not establish comms");
+
+    let resp = serial
+        .send_message(
+            proto::encode(types::inParams::SoundSetSoundType(
+                types::tSoundType::SoundClick,
+            ))
+            .0,
+        )
+        .unwrap();
+
+    println!("{:?}", resp);
+    //sleep(Duration::from_millis(20));
+    //serial
+    //    .write(&proto::encode(types::inParams::MowerAppPause()).0[..])
+    //    .unwrap();
+    //sleep(Duration::from_millis(20));
+
+    //serial.write(&motor(0.3, 0.3)[..]).unwrap();
 }
